0ce69c510c8b6ecb811c427443ba082870c7706a
Marcos G <marcosgomesneto@gmail.com>
2021-09-17 17:36:14 -0300

    Update

--- templates/front/layout/footer.php
+++ templates/front/layout/footer.php
@@ -1,3 +1,7 @@
+<?php
+//Prevent direct file call
+defined( 'ABSPATH' ) || exit;
+?>
 </main>
 <footer class="footer bg-gray-100" class="" style="border-top: 1px solid #dedede;padding: 12px;font-size: 13px;">
     <div class="text-center">
